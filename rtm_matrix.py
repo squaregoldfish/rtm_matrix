c92ec559351d627e770968a90384dbc0fbf0fee8
Steve Jones <steve@squaregoldfish.co.uk>
2025-10-03 09:09:28 +0200

    Seems to be working

--- rtm_matrix.py
+++ rtm_matrix.py
@@ -12,6 +12,8 @@ import pytz
 from tzlocal import get_localzone
 import logging
 from operator import itemgetter
+from Adafruit_LED_Backpack import BicolorMatrix8x8
+import time
 
 RTM_URL = 'https://api.rememberthemilk.com/services/rest/?'
 RATE_LIMIT = 1
@@ -25,6 +27,10 @@ FUTURE = 1
 RED = 0
 YELLOW = 1
 GREEN = 2
+
+# Pre-defined symbols
+NETWORK_ERROR = [[0, 7], [1, 7], [2, 7], [3, 7], [1, 6], [2, 5], [3, 4], [2, 4], [1, 4], [0, 4]]
+GENERIC_ERROR = [[0, 7], [1, 7], [2, 7], [3, 7], [4, 7], [0, 6], [0, 5], [2, 6], [4, 6], [4, 5]]
 
 def midnight(date_object):
     return date_object.replace(hour=0, minute=0, second=0, microsecond=0)
@@ -119,27 +125,98 @@ class rtm():
     def get_tasks(self):
         return self.tasks
 
+def display_symbol(matrix, points, color):
+    for point in points:
+        matrix.set_pixel(point[0], point[1], color)
+
+def display_vertical_binary(matrix, column, number, color):
+    for bit in range(8):
+        if 1 << bit & number:
+            matrix.set_pixel(7 - bit, column, color)
+
+def display_network_error(matrix, status_code):
+    display_symbol(matrix, NETWORK_ERROR, BicolorMatrix8x8.RED)
+
+    hundreds = int(status_code / 100)
+    rest = status_code % 100
+    display_vertical_binary(matrix, 1, hundreds, BicolorMatrix8x8.YELLOW)
+    display_vertical_binary(matrix, 0, rest, BicolorMatrix8x8.YELLOW)
+
+def display_binary_tasks(matrix, count, start_row, color):
+    for bit in range(8):
+        if 1 << bit & count:
+            matrix.set_pixel(start_row, 7 - bit, color)
+
+    for bit in range(8, 16):
+        if 1 << bit & count:
+            matrix.set_pixel(start_row - 1, 7 - bit, color)
+
+def get_row(number):
+    return 7 - int(number / 8)
+
+def get_col(number):
+    return 7 - number % 8
+
+def display_simple_tasks(matrix, overdue, today, future):
+    current_pos = 0
+    for i in range(overdue):
+        matrix.set_pixel(get_row(current_pos), get_col(current_pos), BicolorMatrix8x8.RED)
+        current_pos += 1
+
+    for i in range(current_pos, overdue + today):
+        matrix.set_pixel(get_row(current_pos), get_col(current_pos), BicolorMatrix8x8.YELLOW)
+        current_pos += 1
+
+    for i in range(current_pos, overdue + today + future):
+        matrix.set_pixel(get_row(current_pos), get_col(current_pos), BicolorMatrix8x8.GREEN)
+        current_pos += 1
+
+
+def display_tasks(matrix, overdue, today, future):
+    if overdue + today + future > 64:
+        display_binary_tasks(matrix, overdue, 7, BicolorMatrix8x8.RED)
+        display_binary_tasks(matrix, today, 4, BicolorMatrix8x8.YELLOW)
+        display_binary_tasks(matrix, future, 1, BicolorMatrix8x8.GREEN)
+    else:
+        display_simple_tasks(matrix, overdue, today, future)
+
+
+
 
 if __name__ == "__main__":
     with open('config.toml') as config_file:
         config = toml.load(config_file)
 
+    # Initialise matrix display
+    matrix_config = config['matrix']
+    matrix = BicolorMatrix8x8.BicolorMatrix8x8(address=matrix_config['address'], busnum=matrix_config['i2c_bus'])
+    matrix.begin()
+    matrix.set_brightness(matrix_config['brightness'])
+
     instance = rtm(config['rtm'])
-    instance.fetch_tasks()
 
-    if instance.last_request_status != 200:
-        print('N')
-    elif instance.processing_error:
-        print('E')
-    else:
-        points = list()
+    while True:
+        instance.fetch_tasks()
+        matrix.clear()
 
-        for task in instance.get_tasks():
-            if task['status'] == OVERDUE:
-                points.append(RED)
-            elif task['status'] == TODAY:
-                points.append(YELLOW)
-            else:
-                points.append(GREEN)
-            
-        print(points)
+        if instance.last_request_status != 200:
+            display_network_error(matrix, instance.last_request_status)
+        elif instance.processing_error:
+            display_symbol(matrix, GENERIC_ERROR, BicolorMatrix8x8.RED)
+        else:
+            overdue = 0
+            today = 0
+            future = 0
+
+            for task in instance.get_tasks():
+                if task['status'] == OVERDUE:
+                    overdue += 1
+                elif task['status'] == TODAY:
+                    today += 1
+                else:
+                    future += 1
+
+            display_tasks(matrix, overdue, today, future)
+
+        matrix.write_display()
+        time.sleep(60)
